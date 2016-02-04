95f2786574d7cc8c3c758dc99a753bb098eee4a7
Gerardduch <gerard.duch@est.fib.upc.edu>
2016-02-04 12:38:32 +0100

    heyheyhey

--- routes/authenticate.js
+++ routes/authenticate.js
@@ -14,45 +14,44 @@ var UserModel = mongoose.model('UserModel');
  * usa este token para acceder a las rutas protegidas de la API, es decir,
  * aquellas que requieran de login.
  */
-authRouter.post('/', function(req, res, next) {
-  var id = req.body.identificador;
-  var codeSeg = req.body.codigo_seguridad;
+authRouter.post('/login', function(req, res, next) {
+    var id = req.body.identificador;
+    var codeSeg = req.body.codigo_seguridad;
 
-  // Comprobamos que la nave exista y obtenemos su documento de la BD
-  UserModel.findOne({ identificador: id}, function(err, user) {
-    if (err) res.status(500).send(err);
+    bcrypt.hash(codeSeg, 12, function(error, contraseña_encriptada) {
+        //guardem password encrpt.
+        console.log(contraseña_encriptada);
 
-    if (!user) res.status(401).send('El usuario no existe');
-    else {
-      // Comprobamos el codigo de seguridad
-      if (user.codigo_seguridad != codeSeg) {
-        // Si no son iguales le enviamos un mensaje informando de la situación
-        res.status(401).send('Codigo de seguridad incorrecto');
-      } else {
+        UserModel.findOne({ identificador: id, password: contraseña_encriptada}, function(err, user) {
+            if (err) res.status(500).send(err);
+            
+            if (!user) res.status(401).send('El usuario o la contraseña es incorrecta');
+            else {
+                // Generamos el token con el modulo jwt.
+                // Le pasamos como primer parámetro el documento del usuario
+                // Y como segundo el secret (string que solo concemos nosotros)
+                // Ver fichero config.js para más info
+                console.log(user);
+                var token = jwt.sign(user.toObject(), secret, {
+                    expiresInMinutes: 1440 // Este token expirará en 24h
+                });
 
-        // Generamos el token con el modulo jwt.
-        // Le pasamos como primer parámetro el documento del usuario
-        // Y como segundo el secret (string que solo concemos nosotros)
-        // Ver fichero config.js para más info
-        console.log(user);
-        var token = jwt.sign(user.toObject(), secret, {
-          expiresInMinutes: 1440 // Este token expirará en 24h
+                // Notad que esto nos libra de guardar cualquier estado en el servidor.
+                // Un usuario está autenticado si tiene un token válido que podamos
+                // verificar que hemos dado nosotros (con el middleware express_jwt)
+                // y que no haya expirado. La información del momento exacto de expiración
+                // se guarda en el token en sí, por lo tanto no guardamos ningún estado
+                // en la memoria del servidor.
+
+                // Le enviamos el token al cliente
+                res.json({
+                    token: token
+                });
+            }
         });
 
-        // Notad que esto nos libra de guardar cualquier estado en el servidor.
-        // Un usuario está autenticado si tiene un token válido que podamos
-        // verificar que hemos dado nosotros (con el middleware express_jwt)
-        // y que no haya expirado. La información del momento exacto de expiración 
-        // se guarda en el token en sí, por lo tanto no guardamos ningún estado
-        // en la memoria del servidor.
 
-        // Le enviamos el token al cliente
-        res.json({
-          token: token
-        });
-      }
-    }
-  });
+    });
 });
 
 module.exports = authRouter;
--- routes/register.js
+++ routes/register.js
@@ -5,28 +5,27 @@ var userRouter = require('express').Router();
 var jwt_secret = require('../config').jwt_secret;
 var express_jwt = require('express-jwt');
 
-
-
-
-
 userRouter.post('/login/register', function(req, res, next) {
-    var user = new User(req.body);
-    bcrypt.hash(contraseña_no_encriptada, 12, function(error, contraseña_encriptada) {
+    bcrypt.hash(req.body.password, 12, function(error, contraseña_encriptada) {
         //guardem password encrpt.
         console.log(contraseña_encriptada);
+        var user = new User(req.body);
 
-        //autent
+        user.password = contraseña_encriptada;
+
+        user.save(function(err, newEldar) {
+            if (err) res.status(500).send(err);
+            else res.status(200).json({ user: newUser });
+        });
+
+        /*
         bcrypt.compare('mi-contraseña', contraseña_encriptada, function(err, sonIguales) {
             console.log("asincrono: Deberia ser true: " + sonIguales);
             bcrypt.compare('mi_contraseña', contraseña_encriptada, function(err, sonIguales2) {
                 console.log("asincrono: Deberia ser false: " + sonIguales2);
             });
         });
-    });
-
-    user.save(function(err, newEldar) {
-        if (err) res.status(500).send(err);
-        else res.status(200).json({ user: newUser });
+        */
     });
 });
 
