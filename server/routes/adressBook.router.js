badb0ab2819cf36a3b28b242b3ff8883bfc5846c
Gerardduch <gerard.duch@est.fib.upc.edu>
2016-02-05 11:45:44 +0100

    part login/register de la APIrest acabada

--- server/routes/adressBook.router.js
+++ server/routes/adressBook.router.js
@@ -10,7 +10,7 @@ var UserModel = mongoose.model('UserModel');
 
 
 authRouter.use(express_jwt({ secret: config.JWT_SECRET, requestProperty: 'user' }));
-    // Obtener un usuario y sus agendas
+    // Obtener las agendas de un usuario
     authRouter.get('/', function(req, res) {
         Usuario.findOne({ username: req.usuario.username }, function(err, user) {
             if (err) res.status(500).json(err);
