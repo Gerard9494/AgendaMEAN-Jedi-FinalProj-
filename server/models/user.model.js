71c29ddc0e88b7bdaab92a2ef965826b5d092c6e
Gerardduch <gerard.duch@est.fib.upc.edu>
2016-02-05 11:14:15 +0100

    heyheyhey

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -3,7 +3,7 @@ var Schema = mongoose.Schema;
 
 module.exports = function() {
     var userModelSchema = new Schema({
-        name: {type: String, required: true, index: { unique: true }}, //El name es PK 
+        name: {type: String, required: true, index: { unique: true }}, //El name es PK
         password: {type: String, required: true}
     });
     mongoose.model('UserModel', userModelSchema, 'userModels');
--- server/routes/authenticate.router.js
+++ server/routes/authenticate.router.js
@@ -29,7 +29,8 @@ authRouter.post('/login', function(req, res, next) {
             if (!user) res.status(404).send("El usuario no existe");
             // Sino comparamos la contraseña `a pelo` que nos ha enviado el cliente
             // con la encriptada que habia en la BD
-            else bcrypt.compare(password, user.password, callback);
+            else bcrypt.compare(password, user.password, callback); //esta funcion encripta el password, y luego lo
+                                                                    // compara con el guardado en la bd, que ya esta encriptado
         },
         function(equalPasswords, callback) {
             // Si no son iguales respondemos con error
