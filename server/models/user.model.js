29f928e84cba2abf3140a184c590d12abbfd34a6
Gerardduch <gerard.duch@est.fib.upc.edu>
2016-02-05 11:28:19 +0100

    part login/register de la APIrest acabada

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -4,7 +4,8 @@ var Schema = mongoose.Schema;
 module.exports = function() {
     var userModelSchema = new Schema({
         name: {type: String, required: true, index: { unique: true }}, //El name es PK
-        password: {type: String, required: true}
+        password: {type: String, required: true},
+        adressBooks: [{type: Schema.Types.ObjectId, ref: 'AdressBookModel', default:[]}]
     });
     mongoose.model('UserModel', userModelSchema, 'userModels');
 };
--- server/routes/adressBook.router.js
+++ server/routes/adressBook.router.js
@@ -1,1 +1,19 @@
- 
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+var express_jwt = require('express-jwt');
+var config = require('../config');
+
+var authRouter = require('express').Router();
+
+
+var UserModel = mongoose.model('UserModel');
+
+
+authRouter.use(express_jwt({ secret: config.JWT_SECRET, requestProperty: 'user' }));
+    // Obtener un usuario y sus agendas
+    authRouter.get('/', function(req, res) {
+        Usuario.findOne({ username: req.usuario.username }, function(err, user) {
+            if (err) res.status(500).json(err);
+            else res.status(200).json(user);
+    });
+});
