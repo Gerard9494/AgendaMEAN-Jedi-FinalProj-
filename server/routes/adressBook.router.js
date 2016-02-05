108b0473b29969d95aab354e7238359dda540a6e
Gerardduch <gerard.duch@est.fib.upc.edu>
2016-02-05 15:48:39 +0100

    part login/register de la APIrest acabada

--- server/routes/adressBook.router.js
+++ server/routes/adressBook.router.js
@@ -3,17 +3,32 @@ var bcrypt = require('bcrypt');
 var express_jwt = require('express-jwt');
 var config = require('../config');
 
-var authRouter = require('express').Router();
+var router = require('express').Router();
 
 
 var UserModel = mongoose.model('UserModel');
+var AdressBookModel = mongoose.model('AdressBookModel');
 
+// A partir de aquí todas las rutas van a requerir estar autenticado
+// Por eso usamos el middleware de express_jwt, pasandole el SECRET para
+// que pueda desencriptar el token y comprobar que es correcto
+// Como especificamos requestProperty: 'usuario' en req.usuario tendremos
+// la info del usuario desencriptada
+router.use(express_jwt({ secret: config.JWT_SECRET, requestProperty: 'user' }));
 
-authRouter.use(express_jwt({ secret: config.JWT_SECRET, requestProperty: 'user' }));
-    // Obtener las agendas de un usuario
-    authRouter.get('/', function(req, res) {
-        Usuario.findOne({ username: req.usuario.username }, function(err, user) {
-            if (err) res.status(500).json(err);
-            else res.status(200).json(user);
+// Obtener agendas del usuario
+router.get('/agendas', function(req, res) {
+    UserModel.findOne({ name: req.user.name }, function(err, user) {
+        if (err) res.status(500).json(err);
+        else {
+            var adressBookList = [];
+
+            AdressBookModel.find({ _id: {$in: user.adressBooks} }, function(err, agendas) {
+                if (err) res.status(500).json(err);
+                else res.status(200).json(agendas);
+            });
+
+            res.status(200).json(user.adressBooks);
+        }
     });
 });
