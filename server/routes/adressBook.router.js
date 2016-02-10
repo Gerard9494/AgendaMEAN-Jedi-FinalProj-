86d72dad41f9fe4b2b37cacb0d26af64fcdc8fea
Gerardduch <gerard.duch@est.fib.upc.edu>
2016-02-10 16:31:02 +0100

    crear/modificar/borrar contacte

--- server/routes/adressBook.router.js
+++ server/routes/adressBook.router.js
@@ -10,6 +10,7 @@ var router = require('express').Router();
 
 var UserModel = mongoose.model('UserModel');
 var AdressBookModel = mongoose.model('AdressBookModel');
+var ContactModel = mongoose.model('ContactModel');
 
 // A partir de aquí todas las rutas van a requerir estar autenticado
 // Por eso usamos el middleware de express_jwt, pasandole el SECRET para
@@ -68,21 +69,81 @@ router.patch('/updateAdressBook/:id', function(req, res) {
 // Borrar agenda
 router.delete('/deleteAdressBook/:id', function(req, res) {
     var abId = req.params.id;
-    UserModel.findOne({ name: req.user.name }, function(err, user) {
-        if (err) res.status(500).json(err);
-        else if (!user.adressBooks.contains(abId)) res.status(500).json("No tiene la agenda solicitada");
-        else {
-            var abData = req.body;
-            AdressBookModel.remove({_id: new ObjectId(abId)}, function(err){
-                if(!err) {
-                    res.status(200).end();
-                }
+
+    AdressBookModel.remove({_id: new ObjectId(abId)}, function(err){
+        if(!err) {
+            res.status(200).end();
+        } else {
+            var query = { name: req.usuario.name };
+
+            // Eliminamos la agenda con _id = req.params.id
+            // del array de agendas del usuario
+            var update = { $pull: { "adressBooks._id": req.params.id } };
+
+            // Explicado más arriba
+            var options = { 'new': true };
+
+            UserModel.findOneAndUpdate(query, update, options, function(err, updated) {
+                if (err) res.status(500).json(err);
+                else res.status(200).json(updated);
             });
         }
     });
 });
 
-module.exports = router;
+// Crear contacto
+router.post('/newContact', function(req, res) {
+    var contactInstance = new ContactModel(req.body);
+    contactInstance.save(function(err, newContact) {
+        if (err) res.status(500).send(err);
+        else {
+            ContactModel.save(function(err, newContact) {
+                if (err) res.status(500).send(err);
+                else res.status(200).json({ contact: newContact });
+            });
+        }
+    });
+});
+
+// Modificar contacto
+router.patch('/updateContact/:id', function(req, res) {
+    var cId = req.params.id;
+    var cData = req.body;
+    ContactModel.update({_id: cId}, {$set: cData}, function(err) {
+        if(!err) {
+            res.status(200).end();
+        }
+    });
+});
+
+// Borrar contacto
+router.delete('/deleteContact/:id', function(req, res) { //s'ha de borrar de tot arreu eh!
+    var cId = req.params.id;
+
+    ContactModel.remove({_id: new ObjectId(cId)}, function(err){
+        if(!err) {
+            res.status(200).end();
+        } else {
+            var query = {};
+
+            // Eliminamos la agenda con _id = req.params.id
+            // del array de agendas del usuario
+            var update = { $pull: { "contacts._id": cId } };
+
+            // Explicado más arriba
+            var options = { 'new': true };
+
+            AdressBookModel.findAndUpdate(query, update, options, function(err, updated) {
+                if (err) res.status(500).json(err);
+                else res.status(200).json(updated);
+            });
+        }
+    });
+});
+
+
+// Si no ha entrado en ninguna ruta anterior, error 404 not found
+router.all('*', function(req, res) { res.status(404).send("Recurso no encontrado"); });
 
 
 
