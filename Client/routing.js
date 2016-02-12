ebe1c4b946e560a05cb6e514a5ee26e78efc5de2
Gerardduch <gerard.duch@est.fib.upc.edu>
2016-02-12 16:31:51 +0100

    coses a client

--- Client/routing.js
+++ Client/routing.js
@@ -1,4 +1,4 @@
-angular.module('TareasApp').config(['$stateProvider', '$urlRouterProvider', '$locationProvider',
+angular.module('ContactsApp').config(['$stateProvider', '$urlRouterProvider', '$locationProvider',
     function($stateProvider, $urlRouterProvider, $locationProvider) {
 
         $stateProvider
@@ -12,12 +12,6 @@ angular.module('TareasApp').config(['$stateProvider', '$urlRouterProvider', '$lo
                 templateUrl: 'views/partials/login.html',
                 url: '/login',
                 controller: 'LoginCtrl'
-            })
-            .state('tareas', {
-                parent: 'layout',
-                templateUrl: 'views/partials/tareas.html',
-                url: '/tareas',
-                controller: 'TareasCtrl'
             });
 
         // Cuando la ruta a la que acceda el usuario no sea ninguna
