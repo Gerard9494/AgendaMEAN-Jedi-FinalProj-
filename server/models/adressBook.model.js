b08361d7b1bdf33fbaf6e6a7da58171c54829d40
Gerardduch <gerard.duch@est.fib.upc.edu>
2016-02-13 17:23:43 +0100

    fent coses a client

--- server/models/adressBook.model.js
+++ server/models/adressBook.model.js
@@ -4,6 +4,7 @@ var Schema = mongoose.Schema;
 module.exports = function() {
     var adressBookSchema = new Schema({
         name: {type: String, required: true},
+        email: {type: String, required: true},
         contacts: [{type: Schema.Types.ObjectId, ref: 'ContactModel'}]
     });
     mongoose.model('AdressBookModel', adressBookSchema, 'adressBookModels');
